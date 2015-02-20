bff7882adce6ad822c560d92ff81843eeadf4979
PD1ce <padeisinger@gmail.com>
2015-02-20 13:50:10 -0600

    Updated schools with colors, wrote script for hextodec

--- Accepted/School.swift
+++ Accepted/School.swift
@@ -1,21 +1,24 @@
 //
-//  School.swift
+//  Accepted.swift
 //  Accepted
 //
-//  Created by Philip Deisinger on 2/18/15.
+//  Created by Philip Deisinger on 2/20/15.
 //  Copyright (c) 2015 PDice. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-class School: NSManagedObject {
+class Accepted: NSManagedObject {
 
     @NSManaged var acceptanceRate: NSNumber
     @NSManaged var athleticConference: String
     @NSManaged var attendance: NSNumber
+    @NSManaged var city: String
     @NSManaged var colors: String
+    @NSManaged var endowment: String
     @NSManaged var establishedDate: NSNumber
+    @NSManaged var inStateTuition: NSNumber
     @NSManaged var latitude: NSNumber
     @NSManaged var location: String
     @NSManaged var logoName: String
@@ -23,22 +26,25 @@ class School: NSManagedObject {
     @NSManaged var mascot: String
     @NSManaged var motto: String
     @NSManaged var nickName: String
+    @NSManaged var outOfStateTuition: NSNumber
     @NSManaged var presidentOrChancellor: String
+    @NSManaged var publicPrivate: String
     @NSManaged var schoolName: String
+    @NSManaged var state: String
     @NSManaged var studentsPostgrad: NSNumber
     @NSManaged var studentsTotal: NSNumber
     @NSManaged var studentsUndergrad: NSNumber
     @NSManaged var tuitionGrad: NSNumber
     @NSManaged var tuitionUndergrad: NSNumber
+    @NSManaged var usNewsRanking: NSNumber
     @NSManaged var varsityTeams: NSNumber
     @NSManaged var website: String
-    @NSManaged var city: String
-    @NSManaged var state: String
-    @NSManaged var inStateTuition: NSNumber
-    @NSManaged var usNewsRanking: NSNumber
-    @NSManaged var publicPrivate: String
-    @NSManaged var outOfStateTuition: NSNumber
-    @NSManaged var endowment: String
+    @NSManaged var primaryRed: NSNumber
+    @NSManaged var primaryGreen: NSNumber
+    @NSManaged var primaryBlue: NSNumber
+    @NSManaged var secondaryRed: NSNumber
+    @NSManaged var secondaryGreen: NSNumber
+    @NSManaged var secondaryBlue: NSNumber
     @NSManaged var dream: NSSet
     @NSManaged var favoritedByUsers: NSSet
 
