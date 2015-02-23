3dbbb64f1ad0f9355a03075073a9d191ebccf91d
PD1ce <padeisinger@gmail.com>
2015-02-23 00:14:02 -0600

    Some scroll changes

---
+++ Accepted/SchoolCardTextView.swift
@@ -0,0 +1,15 @@
+//
+//  SchoolCardTextView.swift
+//  Accepted
+//
+//  Created by Philip Deisinger on 2/22/15.
+//  Copyright (c) 2015 PDice. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SchoolCardTextView : UITextView {
+    var position: Int!
+    var open: Bool!
+}
--- Accepted/SchoolViewController.swift
+++ Accepted/SchoolViewController.swift
@@ -14,6 +14,8 @@ class SchoolViewController : UIViewController, UIScrollViewDelegate {
     var school: School!
     var isFavorite: Bool!
     
+    var testBool: Bool!
+    
     @IBOutlet weak var schoolIconImageView: UIImageView!
     @IBOutlet weak var schoolNameLabel: UILabel!
     @IBOutlet weak var schoolLocationLabel: UILabel!
@@ -23,10 +25,12 @@ class SchoolViewController : UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var favoritedLabel: UILabel!
     
     var scrollImageView: UIImageView!
-    var scrollTextView: UITextView!
+    var scrollTextView:  UITextView!
     var scrollTextView2: UITextView!
     var scrollTextView3: UITextView!
     var scrollTextView4: UITextView!
+    
+    var scrollTextViews: [SchoolCardTextView]!
     
     
     override func viewDidLoad() {
@@ -41,51 +45,37 @@ class SchoolViewController : UIViewController, UIScrollViewDelegate {
             println("Does not contain school!")
         }
         
+        scrollTextViews = [SchoolCardTextView]()
+        
         let image = UIImage(named: "universityOfWisconsinMadisonLogo")!
         scrollImageView = UIImageView(image: image)
         scrollImageView.frame = CGRect(origin: CGPointMake(0, 0), size: image.size)
         //mainScrollView.addSubview(scrollImageView)
                 //Data into scrollView
         
+        let gestureRecTap = UITapGestureRecognizer(target: self, action: "textViewTapped")
+        gestureRecTap.numberOfTapsRequired = 1
+        gestureRecTap.numberOfTouchesRequired = 1
         
         
-        scrollTextView = UITextView(frame: CGRect(x: 5, y: 0, width: 200, height: 500))
-        scrollTextView.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1)
-        scrollTextView.layer.cornerRadius = 10.0
-        scrollTextView.text = "ksjdfhaskldjhfaslkhjdg jkshdgkhjlasgjkhsaglkjsahdglkjhsadglkjhsadglkhas ghja sdklhg kalsjdhg kld g h jh klsadhjg lkadsgklhjasd ghjksad glkadslgkjahskljdghaskl dghalskdjhgjklasdhglkjahsghashjkgdsajkhgkljhads hjg kahjlsdg klhjasldg hjdas hj kljdhaskglhasdklhg lkasjdhg hepowughsudbgkjsad bkghljk adshglkj ahshg[weohgucjgadsjhgkjsdhgodvwqnuvnwdufghsalfhgsdjkabgawevaewhgajsdhgkljashfk baslkdbfnwbeufgbpwbgjklasbdgklbafeub weubfkbsd klfb askljdbfklwaefubwepufbasdbfabsd klbfkldhsabfjhab wfuaewfasdlkhfbbd"
-        scrollTextView.layer.borderWidth = 5
-        mainScrollView.addSubview(scrollTextView)
+        for var i = 0; i < 5; i++ {
+            var textView = SchoolCardTextView(frame: CGRect(x: (i * 50) + 5, y: 0, width: 300, height: 500))
+            textView.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1)
+            textView.layer.cornerRadius = 10.0
+            textView.position = i
+            textView.open = false
+            textView.text = "Stuff!"
+            textView.layer.borderWidth = 5
+            textView.editable = false
+            textView.selectable = false
+            textView.addGestureRecognizer(gestureRecTap)
+            scrollTextViews.append(textView)
+            mainScrollView.addSubview(textView)
+        }
         mainScrollView.contentSize = CGSize(width: 375, height: 1000)
         
-        
-        scrollTextView2 = UITextView(frame: CGRect(x: 55, y: 0, width: 200, height: 500))
-        scrollTextView2.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1)
-        scrollTextView2.layer.cornerRadius = 10.0
-        scrollTextView2.text = "ksjdfhaskldjhfaslkhjdg jkshdgkhjlasgjkhsaglkjsahdglkjhsadglkjhsadglkhas ghja sdklhg kalsjdhg kld g h jh klsadhjg lkadsgklhjasd ghjksad glkadslgkjahskljdghaskl dghalskdjhgjklasdhglkjahsghashjkgdsajkhgkljhads hjg kahjlsdg klhjasldg hjdas hj kljdhaskglhasdklhg lkasjdhg hepowughsudbgkjsad bkghljk adshglkj ahshg[weohgucjgadsjhgkjsdhgodvwqnuvnwdufghsalfhgsdjkabgawevaewhgajsdhgkljashfk baslkdbfnwbeufgbpwbgjklasbdgklbafeub weubfkbsd klfb askljdbfklwaefubwepufbasdbfabsd klbfkldhsabfjhab wfuaewfasdlkhfbbd"
-        scrollTextView2.layer.borderWidth = 5
-        mainScrollView.addSubview(scrollTextView2)
-        mainScrollView.contentSize = CGSize(width: 375, height: 1000)
-
-        
-        scrollTextView3 = UITextView(frame: CGRect(x: 105, y: 0, width: 200, height: 500))
-        scrollTextView3.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1)
-        scrollTextView3.layer.cornerRadius = 10.0
-        scrollTextView3.text = "ksjdfhaskldjhfaslkhjdg jkshdgkhjlasgjkhsaglkjsahdglkjhsadglkjhsadglkhas ghja sdklhg kalsjdhg kld g h jh klsadhjg lkadsgklhjasd ghjksad glkadslgkjahskljdghaskl dghalskdjhgjklasdhglkjahsghashjkgdsajkhgkljhads hjg kahjlsdg klhjasldg hjdas hj kljdhaskglhasdklhg lkasjdhg hepowughsudbgkjsad bkghljk adshglkj ahshg[weohgucjgadsjhgkjsdhgodvwqnuvnwdufghsalfhgsdjkabgawevaewhgajsdhgkljashfk baslkdbfnwbeufgbpwbgjklasbdgklbafeub weubfkbsd klfb askljdbfklwaefubwepufbasdbfabsd klbfkldhsabfjhab wfuaewfasdlkhfbbd"
-        scrollTextView3.layer.borderWidth = 5
-        mainScrollView.addSubview(scrollTextView3)
-        mainScrollView.contentSize = CGSize(width: 375, height: 1000)
-
-        
-        scrollTextView4 = UITextView(frame: CGRect(x: 155, y: 0, width: 200, height: 500))
-        scrollTextView4.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1)
-        scrollTextView4.layer.cornerRadius = 10.0
-        scrollTextView4.text = "ksjdfhaskldjhfaslkhjdg jkshdgkhjlasgjkhsaglkjsahdglkjhsadglkjhsadglkhas ghja sdklhg kalsjdhg kld g h jh klsadhjg lkadsgklhjasd ghjksad glkadslgkjahskljdghaskl dghalskdjhgjklasdhglkjahsghashjkgdsajkhgkljhads hjg kahjlsdg klhjasldg hjdas hj kljdhaskglhasdklhg lkasjdhg hepowughsudbgkjsad bkghljk adshglkj ahshg[weohgucjgadsjhgkjsdhgodvwqnuvnwdufghsalfhgsdjkabgawevaewhgajsdhgkljashfk baslkdbfnwbeufgbpwbgjklasbdgklbafeub weubfkbsd klfb askljdbfklwaefubwepufbasdbfabsd klbfkldhsabfjhab wfuaewfasdlkhfbbd"
-        scrollTextView4.layer.borderWidth = 5
-        mainScrollView.addSubview(scrollTextView4)
-        mainScrollView.contentSize = CGSize(width: 375, height: 1000)
-
-        
-
+        //test
+        testBool = false
         
         self.view.backgroundColor = UIColor(red: CGFloat(school.primaryRed), green: CGFloat(school.primaryGreen), blue: CGFloat(school.primaryBlue), alpha: 1)
         let textColor = UIColor(red: CGFloat(school.secondaryRed), green: CGFloat(school.secondaryGreen), blue: CGFloat(school.secondaryBlue), alpha: 1)
@@ -117,18 +107,31 @@ class SchoolViewController : UIViewController, UIScrollViewDelegate {
             favoritedLabel.text = "School Favorited!"
             isFavorite = !isFavorite
         }
-        
-        UIView.animateWithDuration(0.75, delay: 0.0, options: .CurveEaseOut, animations: {
-            self.scrollTextView2.center.x += 100
-            self.scrollTextView3.center.x += 100
-            self.scrollTextView4.center.x += 100
+    }
+    
+    func textViewTapped() {
+        if !testBool {
+            UIView.animateWithDuration(0.75, delay: 0.0, options: .CurveEaseInOut, animations: {
+                self.scrollTextView2.center.x += 200
+                self.scrollTextView3.center.x += 200
+                self.scrollTextView4.center.x += 200
             
-            
-            
-            }, completion: { finished in
+                }, completion: { finished in
                 
+                }
+            )
+            testBool = !testBool
+        } else {
+            UIView.animateWithDuration(0.75, delay: 0.0, options: .CurveEaseInOut, animations: {
+                self.scrollTextView2.center.x -= 200
+                self.scrollTextView3.center.x -= 200
+                self.scrollTextView4.center.x -= 200
                 
-            })
-        
+                }, completion: { finished in
+                    
+                }
+            )
+            testBool = !testBool
+        }
     }
 }
