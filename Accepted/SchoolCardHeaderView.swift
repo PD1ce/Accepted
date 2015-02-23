7b76203b1288fd3c4505105d7c8ffcae6dc605ce
PD1ce <padeisinger@gmail.com>
2015-02-23 13:05:09 -0600

    Same as last

--- Accepted/SchoolCardHeaderView.swift
+++ Accepted/SchoolCardHeaderView.swift
@@ -7,3 +7,8 @@
 //
 
 import Foundation
+import UIKit
+
+class SchoolCardHeaderView : UITextView {
+    var associatedCard: SchoolCardTextView!
+}
--- Accepted/SchoolViewController.swift
+++ Accepted/SchoolViewController.swift
@@ -31,54 +31,153 @@ class SchoolViewController : UIViewController, UIScrollViewDelegate {
     var scrollTextView4: UITextView!
     
     var scrollTextViews: [SchoolCardTextView]!
-    
+    var headerViews: [SchoolCardHeaderView]!
+    var selectedColor: UIColor!
+    var unselectedColor: UIColor!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if user.favoriteSchools.containsObject(school) {
             isFavorite = true
             favButtonView.setImage(UIImage(named: "favSchoolYes"), forState: nil)
-            println("Contains school!")
         } else {
             isFavorite = false
             favButtonView.setImage(UIImage(named: "favSchoolNo"), forState: nil)
-            println("Does not contain school!")
         }
         
+        let backgroundColor = UIColor(red: CGFloat(school.primaryRed), green: CGFloat(school.primaryGreen), blue: CGFloat(school.primaryBlue), alpha: 1)
+        self.view.backgroundColor = backgroundColor
+        let textColor = UIColor(red: CGFloat(school.secondaryRed), green: CGFloat(school.secondaryGreen), blue: CGFloat(school.secondaryBlue), alpha: 1)
+        
+        selectedColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+        unselectedColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.0)
+        
+        mainScrollView.bounces = false
+        
         scrollTextViews = [SchoolCardTextView]()
+        headerViews = [SchoolCardHeaderView]()
         
         let image = UIImage(named: "universityOfWisconsinMadisonLogo")!
         scrollImageView = UIImageView(image: image)
         scrollImageView.frame = CGRect(origin: CGPointMake(0, 0), size: image.size)
         //mainScrollView.addSubview(scrollImageView)
                 //Data into scrollView
+
+        //Instantiating GRs
+        let generalViewTap = UITapGestureRecognizer(target: self, action: "generalViewTapped:")
+        generalViewTap.numberOfTapsRequired = 1
+        generalViewTap.numberOfTouchesRequired = 1
+        let academicViewTap = UITapGestureRecognizer(target: self, action: "academicViewTapped:")
+        academicViewTap.numberOfTapsRequired = 1
+        academicViewTap.numberOfTouchesRequired = 1
+        let athleticViewTap = UITapGestureRecognizer(target: self, action: "athleticViewTapped:")
+        athleticViewTap.numberOfTapsRequired = 1
+        athleticViewTap.numberOfTouchesRequired = 1
+        let studentLifeViewTap = UITapGestureRecognizer(target: self, action: "studentLifeViewTapped:")
+        studentLifeViewTap.numberOfTapsRequired = 1
+        studentLifeViewTap.numberOfTouchesRequired = 1
         
-        let gestureRecTap = UITapGestureRecognizer(target: self, action: "textViewTapped")
-        gestureRecTap.numberOfTapsRequired = 1
-        gestureRecTap.numberOfTouchesRequired = 1
         
+        //General Subview
+        let generalView = SchoolCardHeaderView(frame: CGRect(x: 0, y: 227, width: 75, height: 30))
+        generalView.text = "General"
+        generalView.editable = false
+        generalView.selectable = false
+        generalView.backgroundColor = selectedColor
+        generalView.textAlignment = NSTextAlignment(rawValue: 1)!
+        generalView.font = UIFont(name: "Avenir Heavy", size: 14.0)
+        generalView.textColor = textColor
+        generalView.layer.cornerRadius = 10.0
+        generalView.layer.zPosition = -1
+        generalView.layer.borderWidth = 2
+        generalView.layer.borderColor = textColor.CGColor
+        generalView.addGestureRecognizer(generalViewTap)
+        self.view.addSubview(generalView)
+        headerViews.append(generalView)
+        //Academic Subview
+        let academicView = SchoolCardHeaderView(frame: CGRect(x: 75, y: 227, width: 75, height: 30))
+        academicView.text = "Academics"
+        academicView.editable = false
+        academicView.selectable = false
+        academicView.backgroundColor = unselectedColor
+        academicView.textAlignment = NSTextAlignment(rawValue: 1)!
+        academicView.font = UIFont(name: "Avenir Heavy", size: 14.0)
+        academicView.textColor = textColor
+        academicView.layer.cornerRadius = 10.0
+        academicView.layer.zPosition = -1
+        academicView.layer.borderWidth = 2
+        academicView.layer.borderColor = textColor.CGColor
+        academicView.addGestureRecognizer(academicViewTap)
+        self.view.addSubview(academicView)
+        headerViews.append(academicView)
+        //Academic Subview
+        let athleticView = SchoolCardHeaderView(frame: CGRect(x: 150, y: 227, width: 75, height: 30))
+        athleticView.text = "Athletics"
+        athleticView.editable = false
+        athleticView.selectable = false
+        athleticView.backgroundColor = unselectedColor
+        athleticView.textAlignment = NSTextAlignment(rawValue: 1)!
+        athleticView.font = UIFont(name: "Avenir Heavy", size: 14.0)
+        athleticView.textColor = textColor
+        athleticView.layer.cornerRadius = 10.0
+        athleticView.layer.zPosition = -1
+        athleticView.layer.borderWidth = 2
+        athleticView.layer.borderColor = textColor.CGColor
+        athleticView.addGestureRecognizer(athleticViewTap)
+        self.view.addSubview(athleticView)
+        headerViews.append(athleticView)
+        //StudentLife Subview
+        let studentLifeView = SchoolCardHeaderView(frame: CGRect(x: 225, y: 227, width: 100, height: 30))
+        studentLifeView.text = "Student Life"
+        studentLifeView.editable = false
+        studentLifeView.selectable = false
+        studentLifeView.backgroundColor = unselectedColor
+        studentLifeView.textAlignment = NSTextAlignment(rawValue: 1)!
+        studentLifeView.font = UIFont(name: "Avenir Heavy", size: 14.0)
+        studentLifeView.textColor = textColor
+        studentLifeView.layer.cornerRadius = 10.0
+        studentLifeView.layer.zPosition = -1
+        studentLifeView.layer.borderWidth = 2
+        studentLifeView.layer.borderColor = textColor.CGColor
+        studentLifeView.addGestureRecognizer(studentLifeViewTap)
+        self.view.addSubview(studentLifeView)
+        headerViews.append(studentLifeView)
         
-        for var i = 0; i < 5; i++ {
-            var textView = SchoolCardTextView(frame: CGRect(x: (i * 50) + 5, y: 0, width: 300, height: 500))
+        for var i = 0; i < 4; i++ {
+            var textView = SchoolCardTextView(frame: CGRect(x: 0, y: 0, width: 375, height: 1000))
             textView.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1)
             textView.layer.cornerRadius = 10.0
             textView.position = i
             textView.open = false
-            textView.text = "Stuff!"
-            textView.layer.borderWidth = 5
+            textView.textAlignment = NSTextAlignment(rawValue: 1)!
+            textView.font = UIFont(name: "Avenir Heavy", size: 20)
+            textView.layer.borderWidth = 2
             textView.editable = false
             textView.selectable = false
-            textView.addGestureRecognizer(gestureRecTap)
             scrollTextViews.append(textView)
             mainScrollView.addSubview(textView)
+            
+            if i == 0 {
+                generalView.associatedCard = textView
+                textView.text = "General"
+            } else if i == 1 {
+                academicView.associatedCard = textView
+                textView.text = "Academics"
+            } else if i == 2 {
+                athleticView.associatedCard = textView
+                textView.text = "Athletics"
+            } else if i == 3 {
+                studentLifeView.associatedCard = textView
+                textView.text = "Student Life"
+            }
         }
         mainScrollView.contentSize = CGSize(width: 375, height: 1000)
+        mainScrollView.bringSubviewToFront(scrollTextViews[0])
         
         //test
         testBool = false
         
-        self.view.backgroundColor = UIColor(red: CGFloat(school.primaryRed), green: CGFloat(school.primaryGreen), blue: CGFloat(school.primaryBlue), alpha: 1)
-        let textColor = UIColor(red: CGFloat(school.secondaryRed), green: CGFloat(school.secondaryGreen), blue: CGFloat(school.secondaryBlue), alpha: 1)
+        
         
         schoolNameLabel.textColor = textColor
         schoolLocationLabel.textColor = textColor
@@ -109,29 +208,49 @@ class SchoolViewController : UIViewController, UIScrollViewDelegate {
         }
     }
     
-    func textViewTapped() {
-        if !testBool {
-            UIView.animateWithDuration(0.75, delay: 0.0, options: .CurveEaseInOut, animations: {
-                self.scrollTextView2.center.x += 200
-                self.scrollTextView3.center.x += 200
-                self.scrollTextView4.center.x += 200
-            
-                }, completion: { finished in
-                
-                }
-            )
-            testBool = !testBool
-        } else {
-            UIView.animateWithDuration(0.75, delay: 0.0, options: .CurveEaseInOut, animations: {
-                self.scrollTextView2.center.x -= 200
-                self.scrollTextView3.center.x -= 200
-                self.scrollTextView4.center.x -= 200
-                
-                }, completion: { finished in
-                    
-                }
-            )
-            testBool = !testBool
+    // Gesture Actions
+    func generalViewTapped(gr: UITapGestureRecognizer) {
+        println("tappedG!")
+        for header in headerViews {
+            if header != gr.view {
+                header.backgroundColor = unselectedColor
+            } else {
+                header.backgroundColor = selectedColor
+            }
         }
+        mainScrollView.bringSubviewToFront((gr.view as SchoolCardHeaderView).associatedCard)
+    }
+    func academicViewTapped(gr: UITapGestureRecognizer) {
+        println("tappedAc!")
+        for header in headerViews {
+            if header != gr.view {
+                header.backgroundColor = unselectedColor
+            } else {
+                header.backgroundColor = selectedColor
+            }
+        }
+        mainScrollView.bringSubviewToFront((gr.view as SchoolCardHeaderView).associatedCard)
+    }
+    func athleticViewTapped(gr: UITapGestureRecognizer) {
+        println("tappedAth!")
+        for header in headerViews {
+            if header != gr.view {
+                header.backgroundColor = unselectedColor
+            } else {
+                header.backgroundColor = selectedColor
+            }
+        }
+        mainScrollView.bringSubviewToFront((gr.view as SchoolCardHeaderView).associatedCard)
+    }
+    func studentLifeViewTapped(gr: UITapGestureRecognizer) {
+        println("tappedSL!")
+        for header in headerViews {
+            if header != gr.view {
+                header.backgroundColor = unselectedColor
+            } else {
+                header.backgroundColor = selectedColor
+            }
+        }
+        mainScrollView.bringSubviewToFront((gr.view as SchoolCardHeaderView).associatedCard)
     }
 }
